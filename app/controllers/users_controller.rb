f4133fc4ac1066459060f6d221f9b5834a6517b7
apuri-uc <apuri1@chicagobooth.edu>
2024-07-24 03:01:35 +0000

    updatee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
     @list_of_users = matching_users.order({ :username => :asc })
     render({ :template => "user_templates/index"})
   end
+
+  def show
+    url_username = params.fetch("path_username")
+    matching_username = User.where({ :username => url_username })
+    @the_user = matching_username.first
+
+    render({ :template => "user_templates/show" })
+  end
 end
