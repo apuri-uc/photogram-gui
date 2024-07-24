22cc84289f0df9a70cdee48a952e92d9f2ddeb7a
apuri-uc <apuri1@chicagobooth.edu>
2024-07-24 03:47:16 +0000

    updateee

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,14 @@
+class PhotosController < ApplicationController
+  def index
+  matching_photos = Photo.all
+   @list_of_photos = matching_photos.order({ :created_at => :desc })
+    render({ :template => "photos_templates/index" })
+  end
+
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos.at(0)
+    render({ :template => "photos_templates/show" })
+  end
+end
